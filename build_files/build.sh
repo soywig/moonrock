3249f22dca99b1fcf3b64276c04b7d226d04c150
Lucas Rubio <65956956+soywig@users.noreply.github.com>
2026-04-02 22:37:43 +0200

    remove some flatpaks

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,6 +19,14 @@ enabled-extensions=['dash-to-dock@micxgx.gmail.com', 'logomenu@aryan_k', 'appind
 EOF
 glib-compile-schemas /usr/share/glib-2.0/schemas/
 
+# remove flatpaks
+flatpak uninstall --system --noninteractive \
+  com.github.Matoking.protontricks \
+  com.github.tchx84.Flatseal \
+  com.ranfdev.DistroShelf \
+  com.vysp3r.ProtonPlus \
+  io.github.flattool.Warehouse
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
