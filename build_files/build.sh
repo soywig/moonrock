85dc708486722c92179b7a241773033a15ffcf4b
Lucas Rubio <65956956+soywig@users.noreply.github.com>
2026-04-03 01:02:18 +0200

    lets focus on traditional packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux gnome-shell-extension-dash-to-dock
+dnf5 install -y tmux gnome-shell-extension-dash-to-dock 
+
+dnf5 remove -y waydroid firewall-config btrfs-assistant input-remapper rom-properties
 
 # enable dash to dock
 cat > /usr/share/glib-2.0/schemas/zz0-99-moonrock.gschema.override << 'EOF'
@@ -18,15 +20,6 @@ cat > /usr/share/glib-2.0/schemas/zz0-99-moonrock.gschema.override << 'EOF'
 enabled-extensions=['dash-to-dock@micxgx.gmail.com', 'logomenu@aryan_k', 'appindicatorsupport@rgcjonas.gmail.com', 'user-theme@gnome-shell-extensions.gcampax.github.com', 'gsconnect@andyholmes.github.io', 'blur-my-shell@aunetx', 'hotedge@jonathan.jdoda.ca', 'caffeine@patapon.info', 'add-to-steam@pupper.space', 'restartto@tiagoporsch.github.io', 'compiz-alike-magic-lamp-effect@hermes83.github.com', 'bazaar-integration@kolunmi.github.io']
 EOF
 glib-compile-schemas /usr/share/glib-2.0/schemas/
-
-# block unwanted flatpaks from being installed on first boot 
-cat >> /usr/share/ublue-os/flatpak-blocklist << 'EOF'
-deny com.github.Matoking.protontricks/*
-deny com.github.tchx84.Flatseal/*
-deny com.ranfdev.DistroShelf/*
-deny com.vysp3r.ProtonPlus/*
-deny io.github.flattool.Warehouse/*
-EOF
 
 # Use a COPR Example:
 #
